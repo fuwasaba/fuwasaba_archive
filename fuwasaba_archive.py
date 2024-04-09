67077d44823e8eda7fae91c84ff6b25fa792e73c
Quantum <97796425+Quantum8060@users.noreply.github.com>
2024-04-10 00:40:32 +0900

    Add files via upload
    ダウンロードURLを変更

--- fuwasaba_archive.py
+++ fuwasaba_archive.py
@@ -13,7 +13,7 @@ Static1.pack()
 
 # β3
 def b3download(event):
-    url='https://cdn.discordapp.com/attachments/1226705659022676049/1226708224762318938/3.zip?ex=6625c00f&is=66134b0f&hm=de343276afd5541eb60f9895f8338d8e16363eac29699fb35babb92c2b86e402&'
+    url='https://u.q-mc.xyz/SlrXLh'
     filename='β3.zip'
 
     urlData = requests.get(url).content
@@ -31,7 +31,7 @@ Button.place(x=125, y=50)
 
 # β5
 def b5download(event):
-    url='https://cdn.discordapp.com/attachments/1226705659022676049/1226708226792488960/5.zip?ex=6625c010&is=66134b10&hm=ccd5551aa8ab3cfde8aece590cf827ec583a611fec743e61822cc284156087db&'
+    url='https://u.q-mc.xyz/27BLeG'
     filename='β5.zip'
 
     urlData = requests.get(url).content
@@ -43,14 +43,14 @@ Static1 = tkinter.Label(text=u'ふわ鯖β5期ファイル')
 Static1.place(y=100)
 
 Button = tkinter.Button(text=u'download', width=20)
-Button.bind("<Button-1>",b5download)
+Button.bind("<Button-2>",b5download)
 Button.place(x=125, y=100)
 
 
 
 # β6
 def b6download(event):
-    url='https://cdn.discordapp.com/attachments/1226705659022676049/1226708297260990557/6.zip?ex=6625c021&is=66134b21&hm=61ae36332778283be8e39d2f0abfde084e8c2fb8c087cd2de1b63ee91afb3701&'
+    url='https://u.q-mc.xyz/9iPcZH'
     filename='β6.zip'
 
     urlData = requests.get(url).content
@@ -62,14 +62,14 @@ Static1 = tkinter.Label(text=u'ふわ鯖β6期ファイル')
 Static1.place(y=150)
 
 Button = tkinter.Button(text=u'download', width=20)
-Button.bind("<Button-1>",b6download)
+Button.bind("<Button-3>",b6download)
 Button.place(x=125, y=150)
 
 
 
 # s1
 def s1download(event):
-    url='https://cdn.discordapp.com/attachments/1226705659022676049/1226705733115052105/1.zip?ex=6625bdbd&is=661348bd&hm=6861d521e80b13b5fcd33cf4d3559af171c2793d6c5990ae6199ea23b2fd8db7&'
+    url='https://u.q-mc.xyz/8cD5MB'
     filename='s1.zip'
 
     urlData = requests.get(url).content
@@ -81,7 +81,7 @@ Static1 = tkinter.Label(text=u'ふわ鯖新1期ファイル')
 Static1.place(y=200)
 
 Button = tkinter.Button(text=u'download', width=20)
-Button.bind("<Button-1>",s1download)
+Button.bind("<Button-4>",s1download)
 Button.place(x=125, y=200)
 
 
