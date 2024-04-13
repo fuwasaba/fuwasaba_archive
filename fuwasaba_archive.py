6912d1b87c02c5b60b97faff9d339f6acdbd305b
Quantum <97796425+Quantum8060@users.noreply.github.com>
2024-04-13 15:07:37 +0900

    Add files via upload

--- fuwasaba_archive.py
+++ fuwasaba_archive.py
@@ -1,7 +1,6 @@
 import tkinter
+import threading
 import requests
-import urllib
-import sys
 
 root = tkinter.Tk()
 root.title(u"Fuwasaba archive")
@@ -11,79 +10,57 @@ root.geometry("400x300") # Windowサイズ
 Static1 = tkinter.Label(text=u'過去シーズン構成ダウンロード')
 Static1.pack()
 
-# β3
-def b3download(event):
-    url='https://u.q-mc.xyz/SlrXLh'
-    filename='β3.zip'
-
+def download_file(url, filename):
     urlData = requests.get(url).content
-
-    with open(filename ,mode='wb') as f: # wb でバイト型を書き込める
+    with open(filename ,mode='wb') as f:
         f.write(urlData)
+
+def b3download():
+    url='https://u.q-mc.xyz/8cD5MB'
+    filename='β3.zip'
+    download_file(url, filename)
 
 Static1 = tkinter.Label(text=u'ふわ鯖β3期ファイル')
 Static1.place(y=50)
 
 Button = tkinter.Button(text=u'download', width=20)
-Button.bind("<Button-1>",b3download)
+Button.bind("<Button-1>", lambda event: threading.Thread(target=b3download).start())
 Button.place(x=125, y=50)
 
-
-# β5
-def b5download(event):
+def b5download():
     url='https://u.q-mc.xyz/27BLeG'
     filename='β5.zip'
-
-    urlData = requests.get(url).content
-
-    with open(filename ,mode='wb') as f: # wb でバイト型を書き込める
-        f.write(urlData)
+    download_file(url, filename)
 
 Static1 = tkinter.Label(text=u'ふわ鯖β5期ファイル')
 Static1.place(y=100)
 
 Button = tkinter.Button(text=u'download', width=20)
-Button.bind("<Button-2>",b5download)
+Button.bind("<Button-2>", lambda event: threading.Thread(target=b5download).start())
 Button.place(x=125, y=100)
 
-
-
-# β6
-def b6download(event):
+def b6download():
     url='https://u.q-mc.xyz/9iPcZH'
     filename='β6.zip'
-
-    urlData = requests.get(url).content
-
-    with open(filename ,mode='wb') as f: # wb でバイト型を書き込める
-        f.write(urlData)
+    download_file(url, filename)
 
 Static1 = tkinter.Label(text=u'ふわ鯖β6期ファイル')
 Static1.place(y=150)
 
 Button = tkinter.Button(text=u'download', width=20)
-Button.bind("<Button-3>",b6download)
+Button.bind("<Button-3>", lambda event: threading.Thread(target=b6download).start())
 Button.place(x=125, y=150)
 
-
-
-# s1
-def s1download(event):
+def s1download():
     url='https://u.q-mc.xyz/8cD5MB'
     filename='s1.zip'
-
-    urlData = requests.get(url).content
-
-    with open(filename ,mode='wb') as f: # wb でバイト型を書き込める
-        f.write(urlData)
+    download_file(url, filename)
 
 Static1 = tkinter.Label(text=u'ふわ鯖新1期ファイル')
 Static1.place(y=200)
 
 Button = tkinter.Button(text=u'download', width=20)
-Button.bind("<Button-4>",s1download)
+Button.bind("<Button-4>", lambda event: threading.Thread(target=s1download).start())
 Button.place(x=125, y=200)
-
-
 
 root.mainloop()
